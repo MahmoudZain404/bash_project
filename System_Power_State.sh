6428fbb3c770f4e2971139917909e860e27c6eac
Mahmoud Zain <111197800+MahmoudZain404@users.noreply.github.com>
2024-08-12 22:15:30 +0300

    Update System_Power_State.sh

--- System_Power_State.sh
+++ System_Power_State.sh
@@ -3,26 +3,23 @@
 Power_State()
 {
     echo "$(date) : $0 Power State Service Start Working" >> Application.logs
-    while :; 
-    do
-
-        select choice in  "Shutdown" "Reboot" "Back"
-        do
-            case $choice in 
-            "Shutdown")
-            echo "$(date) : $0 Admin preform a System Shutdown" >> Application.logs.txt
-            shutdown now 
-            ;;
-            "Reboot")
-            echo "$(date) : $0 Admin preform a System Reboot" >> Application.logs.txt
-            reboot
-            ;;
-            "Back")
-            echo "$(date) : $0 Power State Service Ended" >> Application.logs
-            return 0 
-            ;;
-            esac 
-            break 
+    while :; do
+        select choice in  "Shutdown" "Reboot" "Back"; do
+                case $choice in 
+                "Shutdown")
+                echo "$(date) : $0 Admin preform a System Shutdown" >> Application.logs.txt
+                shutdown now 
+                ;;
+                "Reboot")
+                echo "$(date) : $0 Admin preform a System Reboot" >> Application.logs.txt
+                reboot
+                ;;
+                "Back")
+                echo "$(date) : $0 Power State Service Ended" >> Application.logs
+                return 0 
+                ;;
+                esac 
+                break 
             done
     done
 }
