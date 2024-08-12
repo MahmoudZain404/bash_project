167c21eee90401b16802f9b09527774ce8967b92
Mahmoud Zain <mahmoudzain8888@gmail.com>
2024-08-12 22:20:29 +0300

    my commit 	modified:   Application.logs 	new file:   GitHub_Repo_Link.txt 	modified:   System_Power_State.sh

--- System_Power_State.sh
+++ System_Power_State.sh
@@ -3,26 +3,24 @@
 Power_State()
 {
     echo "$(date) : $0 Power State Service Start Working" >> Application.logs
-    while [ true ] ; 
-    do
+    while :;do
 
-        select choice in  "Shutdown" "Reboot" "Back"
-        do
-            case $choice in 
-            "Shutdown")
-            echo "$(date) : $0 Admin preform a System Shutdown" >> Application.logs.txt
-            shutdown now 
-            ;;
-            "Reboot")
-            echo "$(date) : $0 Admin preform a System Reboot" >> Application.logs.txt
-            reboot
-            ;;
-            "Back")
-            echo "$(date) : $0 Power State Service Ended" >> Application.logs
-            return 0 
-            ;;
-            esac 
-            break 
+        select option in  "Shutdown" "Reboot" "Back"; do
+                case $option in 
+                "Shutdown")
+                echo "$(date) : $0 Admin preform a System Shutdown" >> Application.logs.txt
+                shutdown now 
+                ;;
+                "Reboot")
+                echo "$(date) : $0 Admin preform a System Reboot" >> Application.logs.txt
+                reboot
+                ;;
+                "Back")
+                echo "$(date) : $0 Power State Service Ended" >> Application.logs
+                return 0 
+                ;;
+                esac 
+                break 
             done
     done
 }
